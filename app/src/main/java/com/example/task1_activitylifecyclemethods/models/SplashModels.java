504f6a9fc5fb7d60581d83ef6c304b21317dcc1a
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2025-04-21 23:45:02 +0300

    add SplashModels class with properties and methods for data representation; add securityicon.json for animation assets

--- app/src/main/java/com/example/task1_activitylifecyclemethods/models/SplashModels.java
+++ app/src/main/java/com/example/task1_activitylifecyclemethods/models/SplashModels.java
@@ -1,4 +1,36 @@
 package com.example.task1_activitylifecyclemethods.models;
 
 public class SplashModels {
+    int id,image;
+    String title;
+
+    public SplashModels(int id, String title,int image) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
