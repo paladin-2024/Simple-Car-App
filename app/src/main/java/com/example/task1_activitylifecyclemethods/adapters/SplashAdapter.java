f34255a0bef8570cb83a62daeb00a1a296f262d2
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2025-04-21 23:22:50 +0300

    add SplashAdapter class and rename WallpapersModels to SplashModels

---
+++ app/src/main/java/com/example/task1_activitylifecyclemethods/adapters/SplashAdapter.java
@@ -0,0 +1,4 @@
+package com.example.task1_activitylifecyclemethods.adapters;
+
+public class SplashAdapter {
+}
--- app/src/main/java/com/example/task1_activitylifecyclemethods/models/SplashModels.java
+++ app/src/main/java/com/example/task1_activitylifecyclemethods/models/SplashModels.java
@@ -1,4 +1,4 @@
 package com.example.task1_activitylifecyclemethods.models;
 
-public class WallpapersModels {
+public class SplashModels {
 }
